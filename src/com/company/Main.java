a553415c89664fe445795148661ed87ed8d3520e
Sam Neubauer <sam.neubauer@trojans.dsu.edu>
2020-09-23 19:12:58 -0500

    Final Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,7 +22,6 @@ public class Main {
 
     }
 
-
 /** Get CPU time in nanoseconds since the program(thread) started. */
     /**
      * from: http://nadeausoftware.com/articles/2008/03/java_tip_how_get_cpu_and_user_time_benchmarking#TimingasinglethreadedtaskusingCPUsystemandusertime
